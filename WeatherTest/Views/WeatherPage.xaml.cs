bffca452d9bf56b3e5a9bbafa9f0477e82af4848
heitorpaino <heitorpaino@gmail.com>
2017-05-16 20:56:30 -0300

    new test

---
+++ WeatherTest/Views/WeatherPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+using Xamarin.Forms;
+
+namespace WeatherTest.Views
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void getJson(){
+            HttpClient cliente = new HttpClient();
+
+            var data = await cliente.GetStringAsync("http://openweathermap.org/data/2.5/weather?id=3448439&appid=b1b15e88fa797225412429c1c50c122a1");
+            var json = JsonConvert.DeserializeObject(data);
+            System.Diagnostics.Debug.WriteLine(json);
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            this.getJson();
+        }
+    }
+}
