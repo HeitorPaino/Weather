f7a699b20d1e07d98bfd44f4ab8b3f72a5905ddc
heitorpaino <heitorpaino@gmail.com>
2017-05-18 10:34:29 -0300

    WeatherDetail

---
+++ WeatherTest/Views/WeatherDetail.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WeatherTest.Views
+{
+    public partial class WeatherDetail : ContentPage
+    {
+        public WeatherDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
